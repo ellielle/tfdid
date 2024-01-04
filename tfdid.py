aaaa848956543a3bd18578ececac31d3e53d9b13
ellielle <40385743+ellielle@users.noreply.github.com>
2024-01-04 14:38:44 -0500

    fix: argparse parses args and spits out help properly

--- tfdid.py
+++ tfdid.py
@@ -5,26 +5,48 @@ from utils.decorators import log
 
 @log
 def initialize():
+    # The default directory is the current directory the user is in
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+
+    # argparse parser accepts directory, size and interactive arguments
     parser = argparse.ArgumentParser(
-        prog="tfdid", description="A tool to display storage usage", epilog="Beep Boop"
+        prog="tfdid",
+        description="A tool to display storage usage",
+        usage="tfdid [directory] [options]",
+        epilog="Beep Boop",
     )
     parser.add_argument(
-        "directory", metavar="D", default="~", action="store", help="directory to scan"
+        "directory",
+        metavar="directory",
+        default=f"{dir_path}",
+        action="store",
+        help="directory to scan (default: current directory)",
+    )
+    parser.add_argument(
+        "-a",
+        "--all",
+        action="store_true",
+        default=False,
+        help="search hidden files and folders",
     )
     parser.add_argument(
         "-s",
-        metavar="SIZE",
+        "--size",
+        metavar="size",
         action="store",
         default="block",
         choices=["K", "M", "G", "T", "P", "E"],
-        help="scale block size by SIZE (default: %(default)s)",
+        help="scale block size by SIZE (default: %(default)s). Choices: %(choices)s",
     )
     parser.add_argument(
         "-i", "--interactive", action="store_true", default=False, help="start with GUI"
     )
 
     args = parser.parse_args()
-    print(args)
+    print("ARGS :", args)
+    print(os.fspath(dir_path))
+    # TODO better handling of improper usage
+    parser.error("Error here if options are bad")
 
 
 def generate_directory_tree(dir_path):
